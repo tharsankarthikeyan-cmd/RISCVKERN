6bbecd33c7890b449bfc594e1b2d03b1aa74bf87
TharsanKarthikeyan <tharsankarthikeyan@gmail.com>
2026-07-23 13:22:27 +0530

    made ecalls from U-Mode and User Process

--- src/handler.c
+++ src/handler.c
@@ -5,13 +5,18 @@
 #include "handler.h"
 
 void handler_function(uint64_t scause_reg){
-  uint32_t* write_ptr = (uint32_t*)((uintptr_t)PLIC_CLAIM_COMPLETE + 0xFFFFFFC000000000);
-  uint32_t claim = *write_ptr;
-  if(claim == 10){
-    uint8_t char_t = *(volatile uint8_t*)(0x10000000+0xFFFFFFC000000000);
-    uint8_t char_addr[1] = {char_t};
-    ecall_print((uint8_t*)char_addr,1);
-    //*(volatile uint8_t*)(0x10000000) = char_t;
+  if(scause_reg == 8){
+    ecall_print((uint8_t*)"El Psy Kongroo", 14);
   }
-  *write_ptr = claim;
+  else {
+    uint32_t* write_ptr = (uint32_t*)((uintptr_t)PLIC_CLAIM_COMPLETE + 0xFFFFFFC000000000);
+    uint32_t claim = *write_ptr;
+    if(claim == 10){
+      uint8_t char_t = *(volatile uint8_t*)(0x10000000+0xFFFFFFC000000000);
+      uint8_t char_addr[1] = {char_t};
+      ecall_print((uint8_t*)char_addr,1);
+      //*(volatile uint8_t*)(0x10000000) = char_t;
+    }
+    *write_ptr = claim;
+  }
 }
