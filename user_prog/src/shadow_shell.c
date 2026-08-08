8390ebc2aeb1a3fa1fee68f455ce2200bfdd2e9e
TharsanKarthikeyan <tharsankarthikeyan@gmail.com>
2026-08-08 18:44:20 +0530

    Added Ascii ART and refined the shell introduction

--- user_prog/src/shadow_shell.c
+++ user_prog/src/shadow_shell.c
@@ -4,7 +4,11 @@
 extern void sys_print(uint64_t x_coor, uint64_t y_coor, uint8_t* string_ptr, uint64_t length);
 
 void _start(){
-  uint8_t hello[] = "Hello";
-  sys_print(20,20,(uint8_t*)hello,5);
+  sys_print(1,0,(uint8_t*)" ____  _               _                ____  _          _ _  ",61);
+  sys_print(2,0,(uint8_t*)"/ ___|| |__   __ _  __| | _____      __/ ___|| |__   ___| | | ",61);
+  sys_print(3,0,(uint8_t*)"\\___ \\| '_ \\ / _` |/ _` |/ _ \\ \\ /\\ / /\\___ \\| '_ \\ / _ \\ | | ",61);
+  sys_print(4,0,(uint8_t*)" ___) | | | | (_| | (_| | (_) \\ V  V /  ___) | | | |  __/ | | ",61);
+  sys_print(5,0,(uint8_t*)"|____/|_| |_|\\__,_|\\__,_|\\___/ \\_/\\_/  |____/|_| |_|\\___|_|_| ",61);
+  sys_print(6,0,(uint8_t*)"> ",2);
   while(1);
 }
